becb4c32de31509d566072893e0d2f41c959e0ba
Antoine Drouault <antoinedrouault@hotmail.com>
2021-12-09 23:51:03 +0100

    Create page showing all games available

---
+++ src/pages/SelectGamesPage.jsx
@@ -0,0 +1,44 @@
+import { Card, Typography } from "@mui/material";
+import styled from "styled-components";
+
+const StyledSelectGamesPage = styled.article`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  justify-content: space-around;
+
+  & > * {
+    margin-top: 5%;
+    width: 15%;
+    height: 30%;
+  }
+`;
+
+export default function SelectGamesPage() {
+  const games = [
+    { name: "Red Light, Green Light" },
+    { name: "Dalgona" },
+    { name: "Tug of war" },
+    { name: "Even or Odd ?" },
+    { name: "Who is nearest ?" },
+    { name: "Aim for the hole !" },
+  ];
+
+  return (
+    <StyledSelectGamesPage>
+      {games.map((game) => (
+        <Card
+          key={game.name}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <Typography>{game.name}</Typography>
+        </Card>
+      ))}
+    </StyledSelectGamesPage>
+  );
+}
