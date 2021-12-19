25705ceff79c30c605288332bfbdf3ccedaadd84
Antoine Drouault <antoinedrouault@hotmail.com>
2021-12-19 23:45:21 +0100

    Add events to move/stop player

--- src/components/GreenLightRedLight.jsx
+++ src/components/GreenLightRedLight.jsx
@@ -1,4 +1,6 @@
 import { Button } from "@mui/material";
+import { useRef } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 
 const StyledGreenLightRedLight = styled.div`
@@ -19,21 +21,45 @@ const StyledGreenLightRedLight = styled.div`
   & .player {
     position: absolute;
     left: 49%;
-    bottom: 0%;
-    width: 1vw;
-    height: 1vw;
+    bottom: ${(props) => props.distance}%;
+    width: 1%;
+    padding-top: 1%;
+    height: 0;
     background-color: green;
     border-radius: 100%;
   }
 `;
 
 export default function GreenLightRedLight() {
+  const fieldSize = 100;
+
+  const [distance, setDistance] = useState(0);
+
+  const increment = useRef(null);
+
+  const incrementDistance = () => {
+    increment.current = setInterval(() => {
+      setDistance((v) => v + 1);
+    }, 100);
+  };
+
+  const stopIncrementDistance = () => {
+    clearInterval(increment.current);
+  };
+
   return (
-    <StyledGreenLightRedLight>
+    <StyledGreenLightRedLight distance={distance}>
+      <span>Still {fieldSize - distance}m</span>
       <div className="field">
         <div className="player" data-testid="player"></div>
       </div>
-      <Button>Run</Button>
+      <Button
+        onMouseDown={incrementDistance}
+        onMouseUp={stopIncrementDistance}
+        onMouseLeave={stopIncrementDistance}
+      >
+        Run
+      </Button>
     </StyledGreenLightRedLight>
   );
 }
