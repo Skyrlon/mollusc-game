486874ce26bdabf51fe88e858e3c41b51b282bc0
Antoine Drouault <antoinedrouault@hotmail.com>
2021-12-20 23:52:18 +0100

    Add win/loose condition to stop the game

--- src/components/GreenLightRedLight.jsx
+++ src/components/GreenLightRedLight.jsx
@@ -1,6 +1,6 @@
-import { Button } from "@mui/material";
-import { useRef } from "react";
-import { useState } from "react";
+import { Button, Modal } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 
 const StyledGreenLightRedLight = styled.div`
@@ -10,6 +10,19 @@ const StyledGreenLightRedLight = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  & .game-state {
+    position: absolute;
+    top: 0%;
+    left: 45%;
+    font-size: 2rem;
+  }
+
+  & .remaining-distance {
+    position: absolute;
+    top: 5%;
+    right: 25%;
+  }
 
   & .field {
     position: relative;
@@ -31,9 +44,21 @@ const StyledGreenLightRedLight = styled.div`
 `;
 
 export default function GreenLightRedLight() {
-  const fieldSize = 100;
+  const fieldSize = 50;
+
+  const [startPlaying, setStartPlaying] = useState(false);
+
+  const [stillPlaying, setStillPlaying] = useState(false);
+
+  const [won, setWon] = useState(null);
+
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const [timeLeft, setTimeLeft] = useState(10);
 
   const [distance, setDistance] = useState(0);
+
+  const countdown = useRef(null);
 
   const increment = useRef(null);
 
@@ -47,13 +72,63 @@ export default function GreenLightRedLight() {
     clearInterval(increment.current);
   };
 
+  const startPlay = () => {
+    setIsModalOpen(false);
+    countdown.current = setInterval(() => {
+      setTimeLeft((v) => v - 1);
+    }, 1000);
+    setStillPlaying(true);
+    setStartPlaying(true);
+  };
+
+  useEffect(
+    () => {
+      if (timeLeft <= 0 && fieldSize - distance > 0) {
+        setStillPlaying(false);
+        setWon(false);
+        clearInterval(countdown.current);
+      } else if (timeLeft > 0 && fieldSize - distance <= 0) {
+        setStillPlaying(false);
+        setWon(true);
+        clearInterval(countdown.current);
+      }
+    }, // eslint-disable-next-line
+    [timeLeft, distance]
+  );
+
   return (
-    <StyledGreenLightRedLight distance={distance}>
-      <span>Still {fieldSize - distance}m</span>
+    <StyledGreenLightRedLight distance={(distance / fieldSize) * 100}>
+      <Modal open={isModalOpen}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Button variant="outlined" onClick={startPlay}>
+            Start
+          </Button>
+        </Box>
+      </Modal>
+
+      <span className="game-state">
+        {stillPlaying &&
+          startPlaying &&
+          `${Math.floor(timeLeft / 60)}:${timeLeft % 60}`}
+        {won && !stillPlaying && startPlaying && "You have survived"}
+        {!won && !stillPlaying && startPlaying && "You lost"}
+      </span>
+
+      <span className="remaining-distance">Still {fieldSize - distance}m</span>
+
       <div className="field">
         <div className="player" data-testid="player"></div>
       </div>
+
       <Button
+        disabled={distance === fieldSize}
         onMouseDown={incrementDistance}
         onMouseUp={stopIncrementDistance}
         onMouseLeave={stopIncrementDistance}
